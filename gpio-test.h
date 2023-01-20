8808046de821d33e67d9ba6b810c4146bf79f9a3
Angus Jull <angusjull1@gmail.com>
2023-01-20 16:27:02 -0500

    Fix misdeclaration of GPIO pins

--- gpio-test.h
+++ gpio-test.h
@@ -3,9 +3,9 @@
 
 #include "test-global.h"
 
-#define GPIO_15 1;
-#define GPIO_22 1;
-#define GPIO_23 1;
+#define GPIO_15 1
+#define GPIO_22 1
+#define GPIO_23 1
 
 inline uint8_t gpio_get_input(uint8_t pin) {return 1;}
 inline uint8_t gpio_set_output(uint8_t pin, uint8_t value) {return;}
