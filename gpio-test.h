909e3cf729b3c78908781ef05ac47fa27f10f8b4
Angus Jull <angusjull1@gmail.com>
2023-01-20 12:12:05 -0500

    Update gpio-test.h functions
    Added the ouput function and made their definitions match their use in
    deployment files.

--- gpio-test.h
+++ gpio-test.h
@@ -1,5 +1,7 @@
+#include "test-global.h"
 #define GPIO_15 1;
 #define GPIO_22 1;
 #define GPIO_23 1;
 
-inline int gpio_get_input() {return 1;}
+inline uint8_t gpio_get_input(uint8_t pin) {return 1;}
+inline uint8_t gpio_set_output(uint8_t pin, uint8_t value) {return;}
