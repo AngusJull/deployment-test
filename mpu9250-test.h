2fe415ae937e9110d87107b1458f00e56616f2e9
Angus Jull <angusjull1@gmail.com>
2023-01-20 11:44:39 -0500

    Add test structs for sensors

--- mpu9250-test.h
+++ mpu9250-test.h
@@ -1,4 +1,106 @@
-struct mpu9250_desc_t{
+#include "test-global.h"
+
+struct mpu9250_desc_t {
+    /** I2C instance used by this sensor */
+    struct sercom_i2c_desc_t *i2c_inst;
+
+    /** Telemetry service instance */
+    struct telemetry_service_desc_t *telem;
+
+    /** Buffer used for I2C transaction data */
+    uint8_t buffer[MPU9250_BUFFER_LENGTH];
+
+    /** Pointer to buffer to be used when reading samples from sensor, could be
+        a buffer provided by the telemetry service or this driver instances own
+        buffer */
+    uint8_t *telem_buffer;
+
+    /** The millis value when we started waiting for something */
+    uint32_t wait_start;
+
+    /** Values used when averaging samples for self test and offset
+        calibration */
+    int32_t accel_accumulators[3];
+    /** Values used when averaging samples for self test and offset
+        calibration */
+    int32_t gyro_accumulators[3];
+
+    /** Records time of interrupt before a sample is read from the chip */
+    uint32_t next_sample_time;
+
+    uint32_t last_sample_time;
+    int16_t last_accel_x;
+    int16_t last_accel_y;
+    int16_t last_accel_z;
+    int16_t last_gyro_x;
+    int16_t last_gyro_y;
+    int16_t last_gyro_z;
+    int16_t last_temp;
+    int16_t last_mag_x;
+    int16_t last_mag_y;
+    int16_t last_mag_z;
+
+    /** Magnetometer sensitivity adjustment values */
+    uint8_t mag_asa[3];
+
+    uint8_t samples_to_read;
+    uint8_t extra_samples;
+
+    uint8_t samples_left;
+
+    /** Sensor I2C address */
+    uint8_t mpu9250_addr;
 
 
+    /** I2C transaction id */
+    uint8_t t_id;
+
+    uint8_t retry_count;
+
+    /** Value to be loaded into sample rate register to set ODR */
+    uint8_t odr;
+
+    /** ODR for magnetometer */
+    enum ak8963_odr mag_odr:1;
+    /** Full scale range for gyroscope */
+    enum mpu9250_gyro_fsr gyro_fsr:2;
+    /** Full scale range for accelerometer */
+    enum mpu9250_accel_fsr accel_fsr:2;
+    /** LPF bandwidth for gyroscope */
+    enum mpu9250_gyro_bw gyro_bw:7;
+    /** LPF bandwidth for accelerometer */
+    enum mpu9250_accel_bw accel_bw:7;
+
+    /** Driver current state */
+    enum mpu9250_state state:6;
+    /** Driver state to continue to after subsequence */
+    enum mpu9250_state next_state:6;
+
+    /** Flag set to indicate that the drive should wait for the FIFO to fill and
+        read data in larger chunks rather than reading each sample using the
+        interrupt */
+    uint8_t use_fifo:1;
+    /** Flag to indicate that we the register values to be sent in the current
+        state have been marshaled */
+    uint8_t cmd_ready:1;
+    /** Flag to indicate that we are waiting for an I2C transaction */
+    uint8_t i2c_in_progress:1;
+    /** Flag to indicate that we are doing a wait after an I2C transaction is
+        done */
+    uint8_t post_cmd_wait:1;
+    /** Flag to indicate that samples should be subtracted by accumulation
+        sequence */
+    uint8_t acc_subtract:1;
+    /** Bit that indicates the last magnetometer sample is not valid because the
+        magnetic sensor overflowed */
+    uint8_t last_mag_overflow:1;
+    /** Flag to indicate that we currently have buffer checked out from the
+        telemetry service */
+    uint8_t telemetry_buffer_checked_out:1;
+    /** Flag to indicate that an I2C transaction initiated from an interrupt is
+        in progress (separate from i2c_in_progress to avoid affecting FSM) */
+    uint8_t async_i2c_in_progress:1;
 };
+extern int init_mpu9250(struct mpu9250_desc_t *inst);
+
+extern void mpu9250_service(struct mpu9250_desc_t *inst);
--- ms5611-test.h
+++ ms5611-test.h
@@ -1,4 +1,84 @@
-struct ms5611_desc_t {
+/**
+ * @file ms5611.h
+ * @desc Driver for MS5611 barometric pressure sensor
+ * @author Samuel Dewan
+ * @date 2019-06-04
+ * Last Author:
+ * Last Edited On:
+ */
 
+#ifndef ms5611_h
+#define ms5611_h
 
+#include "test-global.h"
+
+enum ms5611_state {
+    MS5611_RESET,
+    MS5611_RESET_WAIT,
+    MS5611_READ_C1,
+    MS5611_READ_C2,
+    MS5611_READ_C3,
+    MS5611_READ_C4,
+    MS5611_READ_C5,
+    MS5611_READ_C6,
+    MS5611_IDLE,
+    MS5611_CONVERT_PRES,
+    MS5611_CONVERT_PRES_WAIT,
+    MS5611_READ_PRES,
+    MS5611_CONVERT_TEMP,
+    MS5611_CONVERT_TEMP_WAIT,
+    MS5611_READ_TEMP,
+    MS5611_FAILED
 };
+
+struct ms5611_desc_t {
+    /** Time of last reading from sensor */
+    uint32_t last_reading_time;
+    /** Temperature compensated pressure read from sensor */
+    int32_t pressure;
+    /** Temperature read from sensor */
+    int32_t temperature;
+    /** Altitude calculated from sensor */
+    float altitude;
+    
+    /** Pressure used as 0 for altitude calculations */
+    float p0;
+    /** Digital pressure value from ADC  */
+    uint32_t d1;
+    /** Digital tempuratue value from ADC */
+    uint32_t d2;
+
+    /** Conversion start time */
+    uint32_t conv_start_time;
+    
+    /** Time between readings of the sensor */
+    uint32_t period;
+    
+    /** Values read from sensor PROM */
+    uint16_t prom_values[6];
+    /** I2C address for sensor */
+    uint8_t address;
+    /** I2C transaction id */
+    uint8_t t_id;
+    /** Current driver state */
+    enum ms5611_state state:4;
+    /** Currently waiting for an I2C transaction to complete */
+    uint8_t i2c_in_progress:1;
+    /** Flag to indicate whether altitude should be calculated */
+    uint8_t calc_altitude:1;
+    /** Flag to indicate whether p0 has been initialized */
+    uint8_t p0_set:1;
+};
+
+/**
+ * Initialize an instance of the MS5611 driver.
+ */
+extern void init_ms5611 (struct ms5611_desc_t *inst);
+/**
+ * Service to be run in each iteration of the main loop.
+ *
+ * @param inst The MS5611 driver instance for which the service should be run
+ */
+extern void ms5611_service (struct ms5611_desc_t *inst);
+
+#endif /* ms5611_h */
